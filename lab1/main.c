ce8e236fcf3c860df6869e9f194d2ebbe7f9db26
KaluginaMarina <marin.kalugin@gmail.com>
2020-03-22 18:28:15 +0300

    [lab1] структура проекта

--- lab1/main.c
+++ lab1/main.c
@@ -1,31 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <getopt.h>
 #include "process.h"
-
-bool parse_int(const char* str, int* var) {
-  char* end;
-  *var = (int)strtol(str, &end, 10);
-  if (end == str) return false;
-  return true;
-}
 
 int main(int argc, char* argv[]) {
 
-  int opt, n;
+    int n = parse_flag(argc, argv);
 
-  while ((opt = getopt(argc, argv, "p:")) != -1) {
-    switch (opt) {
-      case 'p':
-        if (!parse_int(optarg, &n)) {
-          printf("-p parameter must be integter!");
-          return 1;
-        }
-    }
-  }
 
   if (fork() == 0) {
     printf("%d\n", n);
--- lab1/process.h
+++ lab1/process.h
@@ -1,5 +1,10 @@
+#include <unistd.h>
+#include <stdint.h>
+
 typedef struct {
   uint8_t id;
   int     out_pipes[11];      // hardcoded max size 
   int     in_pipes[11];
 } __attribute__((packed)) Process;
+
+int parse_flag(int argc, char* argv[]);
