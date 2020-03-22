c0189852fb6a4b16b9f01443d6e7d2ea0933666d
johnny-keker <giomail.iv@gmail.com>
2020-03-22 19:11:55 +0300

    [lab1] spawning childs

--- lab1/main.c
+++ lab1/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include "pa1.h"
 #include "process.h"
 
 int main(int argc, char *argv[]) {
 
   int n = parse_flag(argc, argv);
+  const int parent_pid = getpid();
 
-
-  if (fork() == 0) {
-    printf("%d\n", n);
-  } else {
-    printf("%d\n", n);
+  for (int i = 0; i < n; ++i) {
+    if (fork() == 0) {
+      printf(log_started_fmt, i + 1, getpid(), parent_pid);
+      printf(log_done_fmt, i + 1);
+    } else {
+      return 0;
+    }
   }
 }
