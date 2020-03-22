2d37ea4f7bc27e1308e58f9cc6f5d4bb2d03e151
KaluginaMarina <marin.kalugin@gmail.com>
2020-03-22 21:51:25 +0300

    [lab1] send_multicast

--- lab1/ipc.c
+++ lab1/ipc.c
@@ -3,6 +3,16 @@
 
 int send(void *self, local_id dst, const Message *msg) {
   Process* process = (Process*) self;
-  write(*(process->channels + dst)[1], msg, sizeof(msg));
+  write(process.channels[dst][1], msg, sizeof(msg));
+  return 0;
+}
+
+int send_multicast(void * self, const Message * msg){
+  Process* process = (Process*) self;
+  for (int i = 0; i < 11; ++i){
+    if (process->channels[i] != -1 && i != process->id){
+      send(process, i, msg);
+    }
+  }
   return 0;
 }
