429fca2885c3e09bc4b6818aa8246b5134fe58a5
johnny-keker <giomail.iv@gmail.com>
2020-03-29 16:52:44 +0300

    [lab1] fixed array out of bounds error

--- lab1/process.h
+++ lab1/process.h
@@ -6,7 +6,7 @@ typedef struct {
   int id;
   int pid;
   int parent_pid;
-  int channels[11][2];
+  int channels[12][2];
 } __attribute__((packed)) Process;
 
 int working(Process p, FILE *file_pipe);
