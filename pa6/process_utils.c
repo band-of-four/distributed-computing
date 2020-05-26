4058bc7df37c85d7fded888ff30b9cc96b464a42
KaluginaMarina <marin.kalugin@gmail.com>
2020-05-26 22:03:22 +0300

    [pa6] навеное, готово

--- pa6/process_utils.c
+++ pa6/process_utils.c
@@ -18,6 +18,11 @@ typedef struct {
 
 timestamp_t req_time = 0;
 
+int forks[11];
+int dirty[11];
+int reqf[11];
+int requested[11] = {0};
+
 int compare(const void *p, const void *q) {
   QueueItem *a1 = (QueueItem *) p;
   QueueItem *a2 = (QueueItem *) q;
@@ -103,7 +108,9 @@ int request_cs(const void * self) {
   message.s_header = header;
   // ------------------------
   for (int i = 1; i <= 11; ++i) {
-    if (p->channels[i][0] != -1 && i != p->id) {
+    if (p->channels[i][0] != -1 && i != p->id && forks[i] == 0 && requested[i] == 0) {
+//      printf("%d request fork from %d\n", p->id, i);
+      requested[i] = 1;
       send(p, i, &message);
     }
   }
@@ -115,13 +122,6 @@ void process_mutex(Process *p) {
   int iter = 1;
   int done_counter = 0;
 
-  int fork[11] = {0};
-  int dirty[11] = {0};
-  int reqf[11] = {0};
-
-  /* отправляем запрос на занятие критической области */
-  request_cs(p);
-
   /* выясняем сколько процессов (может можно как-то лучше) */
   int n = 0;
 
@@ -132,24 +132,40 @@ void process_mutex(Process *p) {
     }
   }
 
-  for (int i = 0; i < 11; ++i) {
+  // Изначальная инициализация вилок
+  // процесс владеет всеми общими вилками процессов с бОльшим номером
+  // и готов их отдать
+  for (int i = 1; i < 11; ++i) {
     if (i == p->id || i > n) {
-      fork[i] = -1;
+      forks[i] = -1;
       dirty[i] = -1;
       reqf[i] = -1;
+    } else if (i > p->id) {
+        forks[i] = 1;
+        dirty[i] = 1;
+        reqf[i] = 0;
+    } else {
+      forks[i] = 0;
+      dirty[i] = 0;
+      reqf[i] = 0;
     }
   }
-  
-  int queue_reply[n];
-  int capacity_reply = 0;
+
+  /* отправляем запрос на занятие критической области */
+//  request_cs(p);
+
   int received_rep = 0;
   Message received_mes;
   for (;;) {
+    if(iter <= iter_max ){
+      request_cs(p);
+    }
     for (int i = 1; i <= 11; ++i) {
 
       /* если все завершили работу -- выходим */
       if (done_counter == n)
         return;
+
       /* если не существует такого процесса -- пропускаем */
       /* ждем сообщение */
       received_mes.s_header.s_type = -1;
@@ -160,28 +176,18 @@ void process_mutex(Process *p) {
 
       /* если получили request отправляем свои данные и записываем в очередь */
       if (received_mes.s_header.s_type == CS_REQUEST) {
-        if ((received_mes.s_header.s_local_time < req_time)
-            || (received_mes.s_header.s_local_time == req_time && i < p->id)
-            || iter > iter_max) {
-          Message message;
-          local_time++;
-          // ------------------------
-          MessageHeader header;
-          header.s_local_time = get_lamport_time();
-          header.s_payload_len = 0;
-          header.s_type = CS_REPLY;
-          header.s_magic = MESSAGE_MAGIC;
-          message.s_header = header;
-          // ------------------------
-          send(p, i, &message);
-        } else {
-          queue_reply[capacity_reply++] = i;
-        }
+//        printf("%d received request from %d\n", p->id, i);
+        reqf[i] = 1;
       }
       /* пришло сообщение об отпускании критической зоны, выселяем процесс из очереди */
       if (received_mes.s_header.s_type == CS_REPLY) {
+        forks[i] = 1;
+        dirty[i] = 0;
+        requested[i] = 0;
+//        printf("%d received reply from %d\n", p->id, i);
         received_rep++;
       }
+
       /* пришло сообщение об окончании работы -- считаем количество работающих потоков */
       if (received_mes.s_header.s_type == DONE)
         done_counter++;
@@ -190,25 +196,69 @@ void process_mutex(Process *p) {
         return;
 
       /* если мы первые в очереди -- делаем работу и отправляем релиз */
-      if (iter <= iter_max && received_rep == n - 1) {
+
+      // подсчет вилок
+      int fork_count = 0;
+      for (int i = 1; i <= n; ++i){
+        if (i != p->id) {
+          if (reqf[i] == 1) {
+            if (forks[i] == 1 && dirty[i] == 1) {
+              forks[i] = 0;
+              dirty[i] = 0;
+              reqf[i] = 0;
+              Message message;
+              local_time++;
+              // ------------------------
+              MessageHeader header;
+              header.s_local_time = get_lamport_time();
+              header.s_payload_len = 0;
+              header.s_type = CS_REPLY;
+              header.s_magic = MESSAGE_MAGIC;
+              message.s_header = header;
+              // ------------------------
+              send(p, i, &message);
+//              printf("%d send fork to %d\n", p->id, i);
+            }
+          }
+          fork_count += forks[i];
+        }
+      }
+//      printf("%d -- fork count %d\n", p->id, fork_count);
+      if (iter <= iter_max && fork_count == n - 1) {
         char *buff = (char *) malloc(strlen(log_loop_operation_fmt) + sizeof(int) * 3);
         sprintf(buff, log_loop_operation_fmt, p->id, iter++, iter_max);
         print(buff);
-        received_rep = 0;
-        for (int k = 0; k < capacity_reply; ++k) {
-          Message message;
-          local_time++;
-          // ------------------------
-          MessageHeader header;
-          header.s_local_time = get_lamport_time();
-          header.s_payload_len = 0;
-          header.s_type = CS_REPLY;
-          header.s_magic = MESSAGE_MAGIC;
-          message.s_header = header;
-          // ------------------------
-          send(p, queue_reply[k], &message);
+
+        for (int k = 1; k <= n; ++k){
+          if (k != p->id){
+            dirty[k] = 1;
+          }
         }
-        capacity_reply = 0;
+
+        for (int i = 1; i <= n; ++i){
+          if (i != p->id) {
+            if (reqf[i] == 1) {
+              if (forks[i] == 1 && dirty[i] == 1) {
+                forks[i] = 0;
+                dirty[i] = 0;
+                reqf[i] = 0;
+                Message message;
+                local_time++;
+                // ------------------------
+                MessageHeader header;
+                header.s_local_time = get_lamport_time();
+                header.s_payload_len = 0;
+                header.s_type = CS_REPLY;
+                header.s_magic = MESSAGE_MAGIC;
+                message.s_header = header;
+                // ------------------------
+                send(p, i, &message);
+//                printf("%d send fork to %d\n", p->id, i);
+              }
+            }
+          }
+        }
+
         if (iter > iter_max) {
           // Отправляем сообщение DONE
           report_done(p);                              /* пишем done */
@@ -216,9 +266,6 @@ void process_mutex(Process *p) {
           printf("Proc %d завершил работу.\n", p->id);
           if (done_counter == n)
             return;
-        } else {
-          request_cs(p);
-          received_rep = 0;
         }
       }
     }
